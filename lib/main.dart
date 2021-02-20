5730975758988e9be22f5c9df68b9300aef9e5a1
James Parrott <japarrott@shaw.ca>
2021-02-19 23:41:03 -0700

    Initial Hello World
    better things to come hopefully

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,6 @@
+import 'package:flutter/material.dart';
+import 'app.dart';
+
 main() {
-  var header = HeaderWidget();
-  var body = BodyWidget();
-  header.renderHeader();
-  body.renderBody();
-}
-
-class HeaderWidget {
-  var height = 100;
-  var title = 'Well....hello there';
-
-  renderHeader() {
-    print('Hello world');
-  }
-}
-
-class BodyWidget {
-  renderBody() {
-    print('Test');
-  }
+  runApp(App());
 }
